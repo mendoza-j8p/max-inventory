8ec26ed7d6481c97527af178444f3a85576f9db4
Jenifer Mendoza <mendoza.j8p@gmail.com>
2023-01-20 08:51:22 +0100

    implemented methods User

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module max-inventory
 
 go 1.19
+
+require (
+	go.uber.org/fx v1.19.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/dig v1.16.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
+)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"max-inventory/internal/entity"
+)
+
+type Repository interface {
+	SaveUser(ctx context.Context, email, name, password string) error
+	GetUserByEmail(ctx context.Context, email string) (*entity.User, error)
+}
+
+type repo struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) Repository {
+	return &repo{
+		db: db,
+	}
+}
